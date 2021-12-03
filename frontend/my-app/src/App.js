5b6b9096f8a8842c90386238141135109db532ab
Anggitwr <agtwahyu23@gmail.com>
2021-12-03 10:48:13 +0700

    menambah my-app

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Welcom from './welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcom />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/my-app/src/welcome.js
@@ -0,0 +1,5 @@
+function Welcom(){
+	return <h1>opo cuk</h1>;
+}
+
+export default Welcom;
